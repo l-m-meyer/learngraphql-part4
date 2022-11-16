37bd4e49a7676cc482d61c7e9f90432b0d48d09d
Lisa Meyer <lisamermey@gmail.com>
2022-11-16 15:02:39 -0500

    Set the return type of the mutation

--- server/src/schema.js
+++ server/src/schema.js
@@ -11,7 +11,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    incrementTrackViews(id: ID!)
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
   }
 
   type IncrementTrackViewsResponse {
